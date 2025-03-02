a1cf858a87f365c53471d1b9c9fb3a194a9de087
Ariyo Joey <ariyojoey@gmail.com>
2025-03-02 03:00:09 +0100

    changes

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-4xl text-center my-8 uppercase">About this Webapp</h1>
-<p>This is a cool demo app that uses Svelte Kit and Tailwind CSS to build a Pokedex web application.</p>
-<p>Developed by Ariyo Joey of Olympus Technologies.</p>
+<h1 class="text-4xl text-center my-8 uppercase font-mono font-bold">About this Webapp</h1>
+<p class="text-xl my-8 uppercase font-mono font-bold">This is a cool demo app that uses Svelte Kit and Tailwind CSS to build a Pokedex web application.</p>
+<p class="text-xl text-center my-8 uppercase font-mono font-normal">Developed by Ariyo Joey of Olympus Technologies.</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,7 +20,7 @@ fetchPokemon();
 	<title>Pokedex by Ariyo Joey</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Pokedex Database</h1>
 <input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Pokemon">
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
     
