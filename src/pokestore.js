62f9638b70a9d68e3100bff42bcb4cbf306c0f3b
Ariyo Joey <ariyojoey@gmail.com>
2025-03-02 02:42:58 +0100

    Initial commit

---
+++ src/pokestore.js
@@ -0,0 +1,64 @@
+import { writable } from 'svelte/store';
+
+// export const pokemon = writable([]);
+// const pokemonDetails = {};
+// let loaded = false;
+
+let loaded = false;
+export const pokemon = writable([]);
+const pokemonDetails = {};
+
+
+// export const fetchPokemon = async () => {
+// 	if (loaded) return;
+// 	const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+// 	const res = await fetch(url);
+// 	const data = await res.json();
+// 	const loadedPokemon = data.results.map((data, index) => ({
+// 		name: data.name,
+// 		id: index + 1,
+// 		image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+// 			index + 1
+// 		}.png`
+// 	}));
+// 	pokemon.set(loadedPokemon);
+// 	loaded = true;
+// };
+
+
+export const fetchPokemon = async () => {
+    if (loaded) return;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json();
+        const loadedPokemon = data.results.map((data, index) => ({
+            name: data.name,
+            id: index + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+        }));
+        pokemon.set(loadedPokemon);
+        loaded = true;
+    } catch (error) {
+        console.error('Failed to fetch Pokemon:', error);
+    }
+};
+
+export const getPokemonById = async (id) => {
+	if (pokemonDetails[id]) return pokemonDetails[id];
+
+	try {
+		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+		const res = await fetch(url);
+		const data = await res.json();
+		pokemonDetails[id] = data;
+		return data;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+};
---
+++ src/routes/about.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-4xl text-center my-8 uppercase">About this Webapp</h1>
+<p>This is a cool demo app that uses Svelte Kit and Tailwind CSS to build a Pokedex web application.</p>
+<p>Developed by Ariyo Joey of Olympus Technologies.</p>
